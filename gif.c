a6329d45d75b21f9ee6321bd0c47d7547ef22b64
Arno Luebke <aiwluebke@gmail.com>
2019-10-09 00:31:11 +0900

    Update gif.c

--- gif.c
+++ gif.c
@@ -1,27 +1,5 @@
-/* This is free and unencumbered software released into the public domain.
-
-   Anyone is free to copy, modify, publish, use, compile, sell, or
-   distribute this software, either in source code form or as a compiled
-   binary, for any purpose, commercial or non-commercial, and by any
-   means.
-
-   In jurisdictions that recognize copyright laws, the author or authors
-   of this software dedicate any and all copyright interest in the
-   software to the public domain. We make this dedication for the benefit
-   of the public at large and to the detriment of our heirs and
-   successors. We intend this dedication to be an overt act of
-   relinquishment in perpetuity of all present and future rights to this
-   software under copyright law.
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-   IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-   OTHER DEALINGS IN THE SOFTWARE.
-
-   For more information, please refer to <http://unlicense.org/> */
+/* Defines a simple gif encoder.
+   See UNLICENSE for copyright notice. */
 
 #include "gif.h"
 
@@ -32,13 +10,13 @@
 
 #define GIF_U16_MAX     0xFFFF
 
+#define gif_true        1
+#define gif_false       0
 
 typedef unsigned int    gif_u32;
 typedef int             gif_bool;
 
-
-#define gif_true        1
-#define gif_false       0
+GIF_STATIC_ASSERT (sizeof (gif_u32) == 4);
 
 
 #ifndef min
@@ -49,7 +27,6 @@ typedef int             gif_bool;
 /* Offsets `ptr` by `n` bytes. */
 #define offset_ptr(ptr, n)                                  \
     ((void *) (((uintptr_t) (ptr)) + n))
-
 
 
 struct gif {
